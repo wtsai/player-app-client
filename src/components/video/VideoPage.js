ae6b68c14adfd61c277a0abfdba5c59d82ac91ee
Wesley Tsai <wesleyboy42@gmail.com>
2017-10-20 19:32:54 +0800

    Autoplay next video when the end of video

--- src/components/video/VideoPage.js
+++ src/components/video/VideoPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Select from 'react-select';
 import Api from '../../lib/Api';
 import Header from '../header/Header';
+import Selector from '../share/Selector';
 import Video from './Video';
 import './VideoPage.css';
 import 'react-select/dist/react-select.css';
@@ -17,7 +17,7 @@ function getVideo(id) {
 class VideoPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {channel: {contents: []}, playingVideo: '', videos: []};
+    this.state = {channel: {contents: []}, playing: false, playingVideo: {}, videos: []};
   }
 
   componentWillMount() {
@@ -25,31 +25,51 @@ class VideoPage extends Component {
       getVideo(this.props.match.params.id).then(
         res => {
           const channel = res.channel;
-          const videos = channel.contents.map(video => {
-            return Object.assign({}, {value: video.youtube_id, label: video.name});
+          const videos = channel.contents.map((video, index) => {
+            return Object.assign({}, {value: video.youtube_id, label: video.name, labelKey: index});
           });
 
-          this.setState({channel, playingVideo: channel.contents[0].youtube_id, videos});
+          this.setState({
+            channel,
+            playingVideo: {
+              value: channel.contents[0].youtube_id,
+              label: channel.contents[0].name,
+              labelKey: 0,
+            },
+            videos,
+          });
         }
       );
     }
   }
 
   handleChange(newValue) {
-    this.setState({playingVideo: newValue.value});
+    this.setState({playing: false, playingVideo: newValue});
+  }
+
+  handleEnded() {
+    const nextPlay = this.state.playingVideo.labelKey+1;
+
+    if(nextPlay < this.state.videos.length) {
+      this.setState({playing: true, playingVideo: this.state.videos[nextPlay]});
+    } else {
+      console.log('The end of this list.');
+    }
   }
 
   render() {
     return (
       <div className="main-VideoPage page">
         <Header title={this.state.channel.name}/>
-        <Video
-          videoId={this.state.playingVideo}
-        />
-        <Select
-          value={this.state.playingVideo}
+        <Selector
+          value={this.state.playingVideo.value}
           options={this.state.videos}
           onChange={this.handleChange.bind(this)}
+        />
+        <Video
+          videoId={this.state.playingVideo.value}
+          onEnded={this.handleEnded.bind(this)}
+          playing={this.state.playing}
         />
       </div>
     );
